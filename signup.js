db79bac4dfa5b47de4c783a6916a7df7245c18ed
minnuambalath <minnuambalath@gmail.com>
2022-04-13 20:17:10 +0400

    version33

--- signup.js
+++ signup.js
@@ -4,16 +4,16 @@ const phonenumber=document.getElementById('phonenumber');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-var strength = {
-	0: "Worst",
-	1: "Bad",
-	2: "Weak",
-	3: "Good",
-	4: "Strong"
-  }
-  //var password = document.getElementById('password');
-  var meter = document.getElementById('password-strength-meter');
-  var text = document.getElementById('password-strength-text');
+// var strength = {
+// 	0: "Worst",
+// 	1: "Bad",
+// 	2: "Weak",
+// 	3: "Good",
+// 	4: "Strong"
+//   }
+//   //var password = document.getElementById('password');
+//   var meter = document.getElementById('password-strength-meter');
+//   var text = document.getElementById('password-strength-text');
   
 
 form.addEventListener('submit', e => {
