1c16fd50fbb2c193afafa18e9a5ae686d732d34d
minnuambalath <minnuambalath@gmail.com>
2022-04-13 20:01:10 +0400

    v3

--- signup.js
+++ signup.js
@@ -5,11 +5,11 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 var strength = {
-	3: "Worst",
-	4: "Bad",
-	5: "Weak",
-	6: "Good",
-	7: "Strong"
+	0: "Worst",
+	1: "Bad",
+	2: "Weak",
+	3: "Good",
+	4: "Strong"
   }
   //var password = document.getElementById('password');
   var meter = document.getElementById('password-strength-meter');
@@ -26,20 +26,68 @@ form.addEventListener('submit', e => {
          e.preventDefault();
      }
 });
-password.addEventListener('input', function() {
-	var val = password.value;
-	var result = zxcvbn(val);
+password.addEventListener('keyup', function() {
+
+	var pwd = password.value;
+  
+	// Reset if password length is zero
+	if (pwd.length === 0) {
+	  document.getElementById("progresslabel").innerHTML = "";
+	  document.getElementById("progress").value = "0";
+	  return;
+	}
+  
+	// Check progress
+	var prog = [/[$@$!%*#?&]/, /[A-Z]/, /[0-9]/, /[a-z]/]
+	  .reduce((memo, test) => memo + test.test(pwd), 0);
+  
+	// Length must be at least 8 chars
+	if(prog > 2 && pwd.length > 7){
+	  prog++;
+	}
+  
+	// Display it
+	var progress = "";
+	var strength = "";
+	switch (prog) {
+	  case 0:
+	  case 1:
+	  case 2:
+		strength = "25%";
+		progress = "25";
+		break;
+	  case 3:
+		strength = "50%";
+		progress = "50";
+		break;
+	  case 4:
+		strength = "75%";
+		progress = "75";
+		break;
+	  case 5:
+		strength = "100% - Password strength is good";
+		progress = "100";
+		break;
+	}
+	document.getElementById("progresslabel").innerHTML = strength;
+	document.getElementById("progress").value = progress;
+  
+  });
+  
+// password.addEventListener('input', function() {
+// 	var val = password.value;
+// 	var result = zxcvbn(val);
   
 	// Update the password strength meter
-	meter.value = result.score;
+	//meter.value = result.score;
   
 	// Update the text indicator
-	if (val !== "") {
-	  text.innerHTML = "Strength: " + strength[result.score]; 
-	} else {
-	  text.innerHTML = "";
-	}
-  });
+// 	if (val !== "") {
+// 	  text.innerHTML = "Strength: " + strength[result.score]; 
+// 	} else {
+// 	  text.innerHTML = "";
+// 	}
+//   });
 				
 function isFormValid(){
     const inputContainers = form.querySelectorAll('.form-control');
